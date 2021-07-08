780bc8976e8669190077f54cf909549f77b94496
Ami Murase <ami.murase.jpn@gmail.com>
2021-07-08 12:45:11 +0900

    add: input, json data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // 今何の写真が表示されているかを管理
+  const [images, setImages] = useState([]);
+  // 検索バーに入れる文字列を管理
+  const [text, setText] = useState('');
+  // 今何の文字列を検索しているかを管理
+  const [query, setQuery] = useState('apple');
+
+  useEffect(() => {
+    console.log('useEffect Success');
+    fetch(
+      `https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setImages(data.results);
+      });
+  }, [query]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setQuery(text);
+    setText('');
+    console.log('onSubmitが呼ばれました');
+  };
   return (
     <div className="App">
+      <div className="main">
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
     </div>
   );
 }
